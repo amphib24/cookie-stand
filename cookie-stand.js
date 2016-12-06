7bc5bde481e0a01d2690b6e69815800f2a04bc8b
JEFF BLANCHARD <BLANCHARDJD19@COMCAST.NET>
2016-12-05 17:51:35 -0800

    Added seatac section" git push origin day1 git status

--- cookie-stand.js
+++ cookie-stand.js
@@ -27,8 +27,6 @@ var firstAndPike = {
  firstAndPike.randomCustHr();
  firstAndPike.cookiesSold();
 
-
-///////////////////////////////////////
 var total = 0;
 var theList = document.getElementById('first and pike list');
 
@@ -45,3 +43,52 @@ for ( var i = 0; i < hoursOpen.length; i ++) {
  var finalCount = document.createElement('li');
  finalCount.textContent = ' Total: ' + total;
  theList.appendChild(finalCount);
+
+
+ ////.////////////////////
+
+ 'use strict';
+ var hoursOpen = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+ var seatacAirport = {
+   minCustHr : 3,
+   maxCustHr : 24,
+   avgCookiesCust : 1.2,
+   custHr : [],
+   cookiesHr : [],
+
+   randomCustHr : function() {
+    for( var i = 0; i < hoursOpen.length; i++) {
+      var  min = Math.ceil(this.minCustHr);
+      var max = Math.floor(this.maxCustHr);
+      var result = Math.floor(Math.random() * (max - min + 1)) + min;
+      this.custHr.push(result);
+    }
+ },
+
+  cookiesSold : function() {
+   for(var i = 0; i < hoursOpen.length; i++){
+     var result2 = Math.round(this.custHr[i] * this.avgCookiesCust);
+     this.cookiesHr.push(result2);
+   }
+ }
+ }
+
+  seatacAirport.randomCustHr();
+  seatacAirport.cookiesSold();
+
+ var total = 0;
+ var theList = document.getElementById('Seatac Airport List');
+
+ for ( var i = 0; i < hoursOpen.length; i ++) {
+    var liEl = document.createElement('li');
+    var currentHr = hoursOpen[i];
+    var totalCkiesSold = seatacAirport.cookiesHr[i];
+    total += totalCkiesSold;
+    var message = currentHr + ' : ' + totalCkiesSold;
+
+    liEl.textContent = message;
+    theList.appendChild(liEl);
+  }
+  var finalCount = document.createElement('li');
+  finalCount.textContent = ' Total: ' + total;
+  theList.appendChild(finalCount);
